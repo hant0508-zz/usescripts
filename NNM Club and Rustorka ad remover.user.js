2f25636e20ec6f831ff041a6319ec383e00d7cd2
hant0508 <hant0508@users.noreply.github.com>
2016-07-23 19:30:30 +0400

    Added NNM mirror

--- NNM Club and Rustorka ad remover.user.js
+++ NNM Club and Rustorka ad remover.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         NNM Club and Rustorka ad remover
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Removes ad banners by marketgid/tovarro on nnmclub.to and rustorka.com (use with Ublock Origin or Adblock Plus)
 // @author       hant0508
 // @include      /^https?://nnmclub.to/*/
+// @include      /^https?://nnm-club.me/*/
 // @include      /^https?://rustorka.com/*/
 // @grant        none
 // ==/UserScript==
