8c22ffbc58ff1e0a9678a98046269a96883cbb75
hant0508 <hant0508@gmail.com>
2016-08-27 02:04:05 +0400

    Added traforet.com banners blocking

--- Etarg ad remover.user.js
+++ Etarg ad remover.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Etarg ad remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Removes ad banners by etarg (use with Ublock Origin or Adblock Plus)
+// @version      0.2
+// @description  Removes ad banners by etarg and traforet (use with Ublock Origin or Adblock Plus)
 // @author       hant0508
 // @include      *
 // @grant        none
@@ -13,22 +13,30 @@ var tries = 0;
 
 function rem()
 {
-    for (var i = snapResults.snapshotLength - 1; i >= 0; i--)
+    for (var i = snapResults1.snapshotLength - 1; i >= 0; i--)
     {
-        var elm = snapResults.snapshotItem(i);
+        var elm = snapResults1.snapshotItem(i);
+        elm.parentNode.removeChild(elm);
+    }
+
+    for (var i = snapResults2.snapshotLength - 1; i >= 0; i--)
+    {
+        var elm = snapResults2.snapshotItem(i);
         elm.parentNode.removeChild(elm);
     }
 }
 
 function xpath()
 {
-    var regex = "/html/body//div[@onclick and ./table/tr/td/a[contains(@href, 'et-code.ru')]]";
-    snapResults = document.evaluate(regex, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    var regex1 = "/html/body//div[@onclick and ./table/tr/td/a[contains(@href, 'et-code.ru')]]";
+    var regex2 = "/html/body//div[a[@class=\"traforet-br-logo\" and contains(@href, 'traforet.com')]]";
+    snapResults1 = document.evaluate(regex1, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    snapResults2 = document.evaluate(regex2, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 
     if (tries <= 20)
     {
         window.setTimeout(xpath,100);
-        if (snapResults.snapshotLength !== 0) tries = 0;
+        if (snapResults1.snapshotLength || snapResults2.snapshotLength) tries = 0;
         tries++;
     }
 
