c68b2202bf1dc9c2c9610b1fb76f1a3003d05e37
hant0508 <hant0508@gmail.com>
2017-01-19 23:11:16 +0400

    Create OMS disable timeout.user.js

---
+++ OMS disable timeout.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         OMS disable timeout
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Disables 30 seconds timer on ru.onlinemschool.com (use with Ublock Origin or Adblock Plus)
+// @author       hant0508
+// @include      http://ru.onlinemschool.com
+// @grant        none
+// ==/UserScript==
+
+window.setTimeout(function(){oms.ads = true;}, 1000);
