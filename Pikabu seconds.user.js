0f829a16605e6194f8a77f61a14cc1841af7fad4
hant0508 <hant0508@gmail.com>
2017-01-23 01:27:40 +0400

    Add files via upload

---
+++ Pikabu seconds.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Pikabu seconds
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Сколько секунд?
+// @author       hant0508
+// @match        http://pikabu.ru/story/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+function getTime() {
+    var post = document.getElementsByClassName('story__date')[0];
+    if (post === undefined)
+    {
+        window.setTimeout(getTime, 1);
+        return;
+    }
+    console.log(post.title);
+    window.addEventListener('load', setTime(post.title), false);
+}
+
+function setTime(postTime) {
+    var comments = document.getElementsByClassName('b-comment__time');
+    for (var i = 0; i < comments.length; ++i)
+    {
+        var time = comments[i].getAttribute('datetime') - postTime;
+        var sec = ' секунд';
+
+        if (time % 100 < 10 || time % 100 > 20) {
+            if (time % 10 == 1) sec += "a";
+            else if (time % 10 < 5) sec += "ы";
+        }
+        comments[i].innerHTML += ' (' + time + sec + ')';
+    }
+}
+
+getTime();
