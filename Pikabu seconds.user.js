4839646a3593c5d12e17e7f3146e9db663dcd935
hant0508 <hant0508@gmail.com>
2017-01-23 17:55:53 +0400

    Ooops, forgot to update @version

--- Pikabu seconds.user.js
+++ Pikabu seconds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pikabu seconds
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Сколько секунд?
 // @author       hant0508
 // @match        http://pikabu.ru/story/*
